c0608bebde7a300192585e51c0ebb4e2ce5d63e7
Vinicios Wentz <wentz.vinicios@gmail.com>
2018-06-11 22:58:01 -0300

    Test are not working, I can't figure out what is wrong, the test acusses that what has been implemented is undefined

--- matrix/matrix.go
+++ matrix/matrix.go
@@ -1,6 +1,44 @@
 package matrix
 
 type Matrix struct {
-	data [][]float64
-	shape []int
+	matrix  [][]float64
+	rows    int
+	columns int
+}
+
+func NewMatrix(m [][]float64) *Matrix {
+	return &Matrix{matrix: m, rows: len(m), columns: len(m[0])}
+}
+
+func (m Matrix) Multiply(o Matrix) *Matrix {
+	if m.columns != o.rows {
+		panic("The number of rows must be equals to the number of rows of the matrix")
+	}
+
+	out := make([][]float64, len(m.matrix))
+	for x := range m.matrix {
+		for y := range o.matrix {
+			out[x] = make([]float64, len(o.matrix))
+			out[x][y] += m.matrix[x][y] * o.matrix[y][x]
+		}
+	}
+
+	return &Matrix{matrix: out, rows: m.rows, columns: m.columns}
+}
+
+func (m Matrix) Compare(o Matrix) bool {
+	for x := range m.matrix {
+		for y := range o.matrix[0] {
+			if m.matrix[x][y] != o.matrix[x][y] {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+// Create a matrix with all elements zeros
+// TODO test this function
+func CreateZeroMatrix(rows, columns int) *Matrix {
+	return &Matrix{matrix: [][]float64{}, rows: rows, columns: columns}
 }
---
+++ matrix/test_matrix.go
@@ -0,0 +1,24 @@
+package matrix
+
+import (
+	"testing"
+	"fmt"
+)
+
+func TestMatrixMultiplication(t *testing.T) {
+	x := NewMatrix([][]float64{{1, 2}, {2, 1}})
+	//y := Matrix{matrix: [][]float64{{4, 2}, {6, 1}}, rows: 2, columns: 2}
+	//result := Matrix{matrix: [][]float64{{16, 4}, {14, 5}}, rows: 2, columns: 2}
+	//
+	fmt.Println(x)
+	//fmt.Println(y)
+	//fmt.Println(result)
+	//
+	//z := x.Multiply(y)
+	//
+	//eq := result.Compare(z)
+	//
+	//if !eq{
+	//	t.Fatal("Not equals")
+	//}
+}
