dea5da6f38a40282e2f1afd37eaff34201cbc84a
Vinicios Wentz <wentz.vinicios@gmail.com>
2018-06-18 20:53:48 -0300

    NewVector function created

--- vector/vector.go
+++ vector/vector.go
@@ -1,6 +1,13 @@
 package vector
 
+// Vector structure represents 1D array
 type Vector struct {
 	vector []float64
-	len int
+	length int
+}
+
+// NewVector returns a 1D array from the given slice
+// NewVector([]float64{1, 2, 3})
+func NewVector(v []float64) *Vector {
+	return &Vector{vector: v, length: len(v)}
 }
