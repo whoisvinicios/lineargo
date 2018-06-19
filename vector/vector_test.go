50ce36b528295e717698ae2afc422eca1abf9150
Vinicios Wentz <wentz.vinicios@gmail.com>
2018-06-19 13:43:36 -0300

    ZeroVector tested and passed

--- vector/vector_test.go
+++ vector/vector_test.go
@@ -10,3 +10,10 @@ func TestVector_NewVector(t *testing.T) {
 		t.Fatal("The v vector are nil")
 	}
 }
+
+func TestVector_ZerozVector(t *testing.T) {
+	v := ZeroVector(3)
+	if !v.Compare(NewVector([]float64{0, 0, 0})) {
+		t.Fatal("The vectors are not equals")
+	}
+}
