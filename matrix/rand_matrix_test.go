9d9770cd21fad7cc421dc39fb4ea082538d0306f
Vinicios Wentz <wentz.vinicios@gmail.com>
2018-06-21 00:04:42 -0300

    rand_matrix_test

--- matrix/rand_matrix_test.go
+++ matrix/rand_matrix_test.go
@@ -4,8 +4,8 @@ import (
 	"testing"
 )
 
-var x = RandMatrix(10000, 10000)
-var y = RandMatrix(10000, 10000)
+var x = RandMatrix(1000, 1000)
+var y = RandMatrix(1000, 1000)
 
 func TestRandMatrix(t *testing.T) {
 	x.Multiply(y)
